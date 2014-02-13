d67e1876d68362f8b6c3a9af6364f5ae6fd6aedf
lo222fj <lo222fj@student.lnu.se>
2014-02-13 21:09:31 +0100

    På gång...

---
+++ 2-1-galleriet/2-1-galleriet/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_2_1_galleriet.Default" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Galleriet</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+        <asp:FileUpload ID="FileUpload" runat="server"  /><br />
+        <asp:Button ID="UploadButton" runat="server" Text="Ladda upp" OnClick="UploadButton_Click" />
+        <div id="showUploadInfo">
+        
+            Filnamn: <asp:Label ID="FileName" runat="server" Text="Label"></asp:Label><br />
+            Filinnehåll: <asp:Label ID="FileContent" runat="server" Text="Label"></asp:Label><br />
+            Filstorlek: <asp:Label ID="FileSize" runat="server" Text="Label"></asp:Label><br />
+            Sökväg: <asp:Label ID="Path" runat="server" Text="Label"></asp:Label>       
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 2-1-galleriet/2-1-galleriet/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2_1_galleriet
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            
+            string upploadedFile;
+            upploadedFile = FileUpload.PostedFile.FileName;
+            string path = Server.MapPath(@"~/Content/Images") +"\\"+upploadedFile;
+            
+            FileName.Text = FileUpload.PostedFile.FileName;
+            FileContent.Text = FileUpload.PostedFile.ContentType;
+            FileSize.Text = FileUpload.PostedFile.ContentLength.ToString();
+            Path.Text = path;
+
+            FileUpload.PostedFile.SaveAs(path);
+            //FileUpload.PostedFile.SaveAs();
+
+            //FileUpload.SaveAs(@"Content/Images");
+
+            //System.IO.File.
+            //FileUpload.FileContent.CopyTo(
+        }
+    }
+}
